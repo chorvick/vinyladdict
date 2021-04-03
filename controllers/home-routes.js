3baeb3c4cbbbb527ac5602d71c378553f7567e16
Brian Flieck <brianflieck@gmail.com>
2021-04-03 13:14:43 -0400

    updated index.js duplication

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,32 +8,31 @@ const withAuth = require('../utils/auth');
 /// home route
 
 router.get("/", withAuth, (req, res) => {
-    // Post.findAll({
-    //     attributes: ["id", "title", "content", "created_at"],
 
+    Post.findAll({
+        attributes: ["id", "title", "description", "created_at"],
+        include: [{
+            model: Comment,
+            attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+            include: {
+                model: User,
+                attributes: ['username'],
+            }
+        },
+        {
+            model: User,
+            attributes: ['username'],
+        }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
 
-    //     model: User,
-    //     attributes: ['username'],
-    // }
-    //     // ]
-    // )
-    //     .then(dbPostData => {
-    //         const posts = dbPostData.map(post => post.get({ plain: true }));
-
-    //         res.render("homepage", { posts, loggedIn: req.session.loggedIn });
-
-    //     })
-
-    //     .catch(err => {
-    //         console.log(err);
-    //         res.status(500).json(err);
-    //     });
-
-    res.render("homepage");
+            res.render("homepage");
+        });
 });
 
 
-//// user allready signed up log in
 
 router.get("/login", (req, res) => {
     if (req.session.loggedIn) {
--- public/js/login.js
+++ public/js/login.js
@@ -1,56 +1,52 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
-
-    // Collect values from the login form
-    const username = document.querySelector('#username-login').value.trim();
+  
+    const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-
-    if (username && password) {
-        // Send a POST request to the API endpoint
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-
-        if (response.ok) {
-            // If successful, redirect the browser to the page we want them to land on to make their post -- the dashboard
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        }
+  
+    if (email && password) {
+      const response = await fetch('/api/user/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in.');
+      }
     }
-};
-
-const signupFormHandler = async (event) => {
+  };
+  
+  const signupFormHandler = async (event) => {
     event.preventDefault();
-
+  
     const username = document.querySelector('#username-signup').value.trim();
-
+    // const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-
+  
     if (username && password) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        }
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to sign up.');
+      }
     }
-};
-
-document
+  };
+  
+  document
     .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-
-document
     .querySelector('.signup-form')
+    .addEventListener('submit', loginFormHandler)
     .addEventListener('submit', signupFormHandler);
+    
+  
 
-
-    //// leaving as is for now will have to direct them to proper place after login
+  
